89f8e6090b3284be4af65f157badbc54e60483fd
em3rc0d <farid.merino1673@gmail.com>
2024-10-22 18:06:45 -0500

    deleted testjs

--- test.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-// Cargar las variables de entorno
-dotenv.config();
-
-// Imprimir la variable de entorno
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-
